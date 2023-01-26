86ec1fcd5da290f0db0dab80236dab7317a4ad3b
nilmo-contreras <noc2@njit.edu>
2023-01-26 13:06:39 -0500

    Added counter functionality

--- app/src/main/java/com/example/simplecounter/MainActivity.kt
+++ app/src/main/java/com/example/simplecounter/MainActivity.kt
@@ -3,17 +3,23 @@ package com.example.simplecounter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    var counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val textView = findViewById<TextView>(R.id.textView)
         val button = findViewById<Button>(R.id.button)
 
         button.setOnClickListener {
-            Toast.makeText(it.context, "clicked the button!", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(it.context, "clicked the button!", Toast.LENGTH_SHORT).show()
+            counter++
+            textView.text = counter.toString()
         }
     }
 }
