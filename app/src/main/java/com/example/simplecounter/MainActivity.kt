273f1174a18b54b50ab5cdffde3ebce52c865e76
nilmo-contreras <noc2@njit.edu>
2023-01-26 13:01:38 -0500

    Initial commit

---
+++ app/src/main/java/com/example/simplecounter/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.simplecounter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            Toast.makeText(it.context, "clicked the button!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
