163de8d104aaced2ea1ade4dbc8ff86624483fe1
nilmo-contreras <noc2@njit.edu>
2023-01-26 16:25:08 -0500

    Minor UI changes and added upgrade stretch goal

--- app/src/main/java/com/example/simplecounter/MainActivity.kt
+++ app/src/main/java/com/example/simplecounter/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.simplecounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -15,11 +16,30 @@ class MainActivity : AppCompatActivity() {
 
         val textView = findViewById<TextView>(R.id.textView)
         val button = findViewById<Button>(R.id.button)
+        val upgradeButton = findViewById<Button>(R.id.upgradeBtn)
 
         button.setOnClickListener {
             //Toast.makeText(it.context, "clicked the button!", Toast.LENGTH_SHORT).show()
             counter++
             textView.text = counter.toString()
+
+            if (counter >= 100) {
+
+                // Show upgrade button and set onClickListener
+                upgradeButton.visibility = View.VISIBLE
+                upgradeButton.setOnClickListener {
+                    button.text = "Add 2"
+
+                    // Update original button to add 2 instead of `
+                    button.setOnClickListener {
+                        counter += 2
+                        textView.text = counter.toString()
+                    }
+
+                    // Hide upgrade button again
+                    upgradeButton.visibility = View.INVISIBLE
+                }
+            }
         }
     }
 }
